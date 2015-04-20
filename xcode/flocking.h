354cc1d083a15fadaa3e36f883d9e66f64bbcffa
lyyvalhalla <lyy.yiyang@gmail.com>
2015-04-20 03:45:47 -0400

    AOGP

---
+++ xcode/flocking.h
@@ -0,0 +1,37 @@
+//
+//  flocking.h
+//  flockingAPI_one
+//
+//  Created by valhalla on 10/17/14.
+//
+//
+
+#ifndef flockingAPI_one_flocking_h
+#define flockingAPI_one_flocking_h
+
+
+#endif
+
+
+#pragma once
+#include "cinder/app/AppBasic.h"
+#include "cinder/Vector.h"
+#include "cinder/ImageIO.h"
+#include "cinder/Utilities.h"
+#include "cinder/Camera.h"
+#include "cinder/Rand.h"
+#include <vector>
+#include <list>
+
+namespace cinder {
+    
+class Flocking {
+    public:
+    Flocking();
+    Flocking();
+    
+    protected:
+    void particle();
+                 
+}；
+}
